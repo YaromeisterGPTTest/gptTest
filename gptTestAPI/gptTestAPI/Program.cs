c94a5f274d782778678bf857db9f32bd20985af0
Arsen Yaremyn <arsen.yaremyn@nethealth.com>
2023-08-29 16:41:52 +0300

    UC#1 Integration

--- gptTestAPI/gptTestAPI/Program.cs
+++ gptTestAPI/gptTestAPI/Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddCors();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -15,6 +15,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(builder => builder.AllowAnyOrigin()
+                               .AllowAnyMethod()
+                               .AllowAnyHeader());
 
 app.UseHttpsRedirection();
 
