19a6eb3c7fd815c0c7cb66e1a478dd8b58568940
Arsen Yaremyn <arsen.yaremyn@nethealth.com>
2023-08-29 13:05:39 +0300

    UC#1 Create filter function by name

--- gptTestAPI/gptTestAPI/Controllers/CountryController.cs
+++ gptTestAPI/gptTestAPI/Controllers/CountryController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
+using gptTestAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 
 [ApiController]
@@ -10,13 +12,28 @@ public class CountryController : ControllerBase
     private const string ApiUrl = "https://restcountries.com/v3.1/all";
 
     [HttpGet]
-    public async Task<IActionResult> GetCountries(string param1 = null, int? param2 = null, string param3 = null, int? param4 = null)
+    public async Task<IActionResult> GetCountries(string? param1 = null, int? param2 = null, string? param3 = null, int? param4 = null)
     {
         using var httpClient = new HttpClient();
         var response = await httpClient.GetStringAsync(ApiUrl);
 
-        // Do something with the parameters and countries data if needed
+        response = FilterCountriesByString(param1, response);
 
         return Ok(response);
+    }
+
+    private string FilterCountriesByString(string queryString, string jsonCountries)
+    {
+        if (string.IsNullOrEmpty(queryString))
+        {
+            return jsonCountries;
+        }
+        List<Country> countries = JsonSerializer.Deserialize<List<Country>>(jsonCountries, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+
+        queryString = queryString.ToLower();
+
+        var filteredCountries = countries.Where(country => country.Name.Common.ToLower().Contains(queryString)).ToList();
+
+        return JsonSerializer.Serialize(filteredCountries);
     }
 }
