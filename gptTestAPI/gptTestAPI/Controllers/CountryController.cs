2bf92a9ef7560d3efc89b0a2a0673260cc98b4de
Arsen Yaremyn <arsen.yaremyn@nethealth.com>
2023-08-29 13:42:39 +0300

    UC#1 Create filter function by name

--- gptTestAPI/gptTestAPI/Controllers/CountryController.cs
+++ gptTestAPI/gptTestAPI/Controllers/CountryController.cs
@@ -5,35 +5,39 @@ using System.Threading.Tasks;
 using gptTestAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 
-[ApiController]
-[Route("api/[controller]")]
-public class CountryController : ControllerBase
+
+namespace controllers
 {
-    private const string ApiUrl = "https://restcountries.com/v3.1/all";
-
-    [HttpGet]
-    public async Task<IActionResult> GetCountries(string? param1 = null, int? param2 = null, string? param3 = null, int? param4 = null)
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CountryController : ControllerBase
     {
-        using var httpClient = new HttpClient();
-        var response = await httpClient.GetStringAsync(ApiUrl);
+        private const string ApiUrl = "https://restcountries.com/v3.1/all";
 
-        response = FilterCountriesByString(param1, response);
-
-        return Ok(response);
-    }
-
-    private string FilterCountriesByString(string queryString, string jsonCountries)
-    {
-        if (string.IsNullOrEmpty(queryString))
+        [HttpGet]
+        public async Task<IActionResult> GetCountries(string? param1 = null, int? param2 = null, string? param3 = null, int? param4 = null)
         {
-            return jsonCountries;
+            using var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync(ApiUrl);
+
+            response = FilterCountriesByString(param1, response);
+
+            return Ok(response);
         }
-        List<Country> countries = JsonSerializer.Deserialize<List<Country>>(jsonCountries, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
 
-        queryString = queryString.ToLower();
+        private string FilterCountriesByString(string queryString, string jsonCountries)
+        {
+            if (string.IsNullOrEmpty(queryString))
+            {
+                return jsonCountries;
+            }
+            List<Country> countries = JsonSerializer.Deserialize<List<Country>>(jsonCountries, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
 
-        var filteredCountries = countries.Where(country => country.Name.Common.ToLower().Contains(queryString)).ToList();
+            queryString = queryString.ToLower();
 
-        return JsonSerializer.Serialize(filteredCountries);
+            var filteredCountries = countries.Where(country => country.Name.Common.ToLower().Contains(queryString)).ToList();
+
+            return JsonSerializer.Serialize(filteredCountries);
+        }
     }
 }
